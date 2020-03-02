13961766fd99fd6ed8bc2a7c1c4a53c9981981ea
Sana <57024336+SanaBilal@users.noreply.github.com>
2020-03-03 10:29:16 +1100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "./app/Automation.php";
-$output_directory_path = getcwd().'/output';
-$automation = new Automation($output_directory_path);
-$automation->initializer();
-$automation->add_manufacturers();
-?>
